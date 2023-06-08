1bc96ab113b48d36dc322b72aba8fce7ec1c59ca
AR2302 <87814445+AR2302@users.noreply.github.com>
2023-06-08 03:07:50 -0700

    Three game over messages and chocolate bomb execution

--- CS174A_Project/Project/project.js
+++ CS174A_Project/Project/project.js
@@ -27,12 +27,7 @@ export class Project extends Scene {
             bird: new  defs.Subdivision_Sphere(4),
             field: new Cube(),
             seed: new  defs.Subdivision_Sphere(4),
-            sky: new defs.Subdivision_Sphere(4),
-
-            custom_bird: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(4),
-            tri: new defs.Triangle(),
-            //tet: new defs.Tetrahedron(false),
-                
+            sky: new defs.Subdivision_Sphere(3),
         }
 
         // Cannon Variables
@@ -71,13 +66,9 @@ export class Project extends Scene {
                 ambient: 1,diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/birds.jpg")
             }),
-
-            custom_bird_texture: new Material(new defs.Phong_Shader(),                
-                {ambient: 1, diffusivity: 0, color: hex_color("#d90404")}),
-        
             field_texture: new Material(new Textured_Phong(),{
                 color: hex_color("#000000"),
-                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                ambient: 1,diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/field.png")
             }),
             seed_texture: new Material(new Textured_Phong(),{
@@ -106,7 +97,7 @@ export class Project extends Scene {
         
         //counting birds, food
         this.add_text = new add_text(this);
-        this.remaining_bird = 2;
+        this.remaining_bird = 3;
         this.food_count = 3;
         
         this.box_list = [];
@@ -123,6 +114,7 @@ export class Project extends Scene {
         this.seed_transform_list = [];
         this.seed_keep_list = [0,1,2];
         this.hit_seed = false;
+        this.reloaded = false;
     }
 
     make_control_panel() {
@@ -132,13 +124,13 @@ export class Project extends Scene {
             if(!(this.launch)) {
                 this.rotationX += Math.PI / 75.0;
             }
-            if(this.rotationX < 0)
+            if(this.rotationX<0)
             {
-                this.rotationX = 0;
+                this.rotationX=0;
             }
-            if(this.rotationX > Math.PI/2)
+            if(this.rotationX>Math.PI/2)
             {
-                this.rotationX = Math.PI/2;
+                this.rotationX=Math.PI/2;
             }
         });
         this.key_triggered_button("Rotate Down", ["k"], () => {
@@ -146,13 +138,13 @@ export class Project extends Scene {
             if(!(this.launch)) {
                 this.rotationX -= Math.PI / 75.0;
             }
-            if(this.rotationX < 0)
+            if(this.rotationX<0)
             {
-                this.rotationX = 0;
+                this.rotationX=0;
             }
-            if(this.rotationX > Math.PI/2)
+            if(this.rotationX>Math.PI/2)
             {
-                this.rotationX = Math.PI/2;
+                this.rotationX=Math.PI/2;
             }
         });
         this.key_triggered_button("Rotate Left", ["j"], () => {
@@ -160,13 +152,13 @@ export class Project extends Scene {
             if(!(this.launch)) {
                 this.rotationY += Math.PI / 75.0;
             }
-            if(this.rotationY < -Math.PI/2)
+            if(this.rotationY<-Math.PI/2)
             {
-                this.rotationY =- Math.PI/2;
+                this.rotationY=-Math.PI/2;
             }
-            if(this.rotationY > Math.PI/2)
+            if(this.rotationY>Math.PI/2)
             {
-                this.rotationY = Math.PI/2;
+                this.rotationY=Math.PI/2;
             }
         });
         this.key_triggered_button("Rotate Right", ["l"], () => {
@@ -174,15 +166,15 @@ export class Project extends Scene {
 
             if(!(this.launch))
             {
-                this.rotationY -= Math.PI / 75.0;
+                this.rotationY-=Math.PI / 75.0;
             }
-            if(this.rotationY < -Math.PI/2)
+            if(this.rotationY<-Math.PI/2)
             {
-                this.rotationY =- Math.PI/2;
+                this.rotationY=-Math.PI/2;
             }
-            if(this.rotationY > Math.PI/2)
+            if(this.rotationY>Math.PI/2)
             {
-                this.rotationY = Math.PI/2;
+                this.rotationY=Math.PI/2;
             }
         });
         this.key_triggered_button("launch", ["q"], () => {
@@ -197,7 +189,6 @@ export class Project extends Scene {
 
         });
         this.key_triggered_button("reload", ["e"], () => {
-            // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off
             if (!this.game_over){
                 this.launch=false;
                 this.moving=false;
@@ -211,11 +202,11 @@ export class Project extends Scene {
                 this.hit_chocolate = false;
                 this.hit_box = false;
                 this.hit_seed = false;
+                this.reloaded = true;
             }
 
         });
         this.key_triggered_button("increase power", ["u"], () => {
-            // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and of
             if(!this.game_over){
                 if(!this.launch)
                 {
@@ -229,7 +220,6 @@ export class Project extends Scene {
 
         });
         this.key_triggered_button("decrease power", ["y"], () => {
-            // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off
             if(!this.game_over){
                 if(!this.launch) {
                     this.cannon_power-=1;
@@ -243,10 +233,9 @@ export class Project extends Scene {
         });
         
         this.key_triggered_button("Restart", ["m"], () => {
-            // TODO:  Requirement 5b:  Set a flag here that will toggle your outline on and off
             //if restart is false, restart = true
             if (!this.restart){
-                this.remaining_bird = 2;
+                this.remaining_bird = 3;
                 this.food_count = 3;
                 this.launch=false;
                 this.moving=false;
@@ -264,6 +253,7 @@ export class Project extends Scene {
                 this.box_keep_list = [0,1,2,3,4,5,6,7,8];
                 this.hit_seed = false;
                 this.seed_keep_list = [0,1,2];
+                this.reloaded = false;
             }
 
         });
@@ -327,12 +317,12 @@ export class Project extends Scene {
         let t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
         let model_transform = Mat4.identity();
 
-        // TODO:  Draw the required boxes. Also update their stored matrices.
+
         // draw background
         this.sky_transform = Mat4.identity();
         this.sky_transform = this.sky_transform.times(Mat4.scale(60,60,60));
         this.shapes.sky.draw(context, program_state, this.sky_transform, this.materials.sky_texture);
-         
+
         //draw box
         const arrayColumn = (arr, n) => arr.map(x=>x[n]);
         this.box_list = [];
@@ -416,65 +406,6 @@ export class Project extends Scene {
             this.shapes.seed.draw(context, program_state, a, this.materials.seed_texture);
         }
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // Custom Bird model
-        // Change X, Y, Z coords to move the bird
-        let custom_bird_x = 0;
-        let custom_bird_y = 5;
-        let custom_bird_z = 0;
-        // red body
-        this.bird_transform = Mat4.identity();
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.scale(0.3,0.3,0.3));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture);
-        // white belly
-        this.bird_transform = this.bird_transform.times(Mat4.translation(0, -0.07, -0.07)).times(Mat4.scale(0.9, 1, 0.9));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("ffffff")}));
-        this.bird_transform = Mat4.identity();
-        // Left Eye 
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(-0.06, 0, -0.2)).times(Mat4.scale(0.12, 0.12, 0.11));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("ffffff")}));
-        this.bird_transform = this.bird_transform.times(Mat4.translation(-0.09, -0.37, -0.86)).times(Mat4.scale(0.14, 0.14, 0.14));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("000000")}));
-        this.bird_transform = Mat4.identity();
-        // Right Eye
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(0.06, 0, -0.2)).times(Mat4.scale(0.12, 0.12, 0.11));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("ffffff")}));
-        this.bird_transform = this.bird_transform.times(Mat4.translation(0.09, -0.37, -0.86)).times(Mat4.scale(0.14, 0.14, 0.14));
-        this.shapes.custom_bird.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("000000")}));
-        this.bird_transform = Mat4.identity();
-        // Eyebrows
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(0, 0.1, -0.28)).times(Mat4.scale(0.2, 0.022, 0.022));
-        this.shapes.box_1.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("000000")}));
-        this.bird_transform = Mat4.identity();
-        // Hair On Top
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(0, 0.29, 0)).times(Mat4.scale(0.022, 0.1, 0.022));
-        this.shapes.box_1.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("d90404")}));
-        this.bird_transform = this.bird_transform.times(Mat4.rotation(0.3, 0.5, 1.3, 1)).times(Mat4.translation(-3, 0.5, 1));
-        this.shapes.box_1.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("d90404")}));
-        this.bird_transform = this.bird_transform.times(Mat4.rotation(0.3, -0.5, -1.1, 1)).times(Mat4.translation(4.7, -1.5, 1));
-        this.shapes.box_1.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("d90404")}));
-        
-        // Beak (WIP)
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(0, -0.1, -0.3)).times(Mat4.scale(0.11, 0.06, 1));
-        this.bird_transform = this.bird_transform.times(Mat4.rotation(0, 0, 0, 1));
-        this.shapes.tri.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("#fcba03")}));
-        this.bird_transform = Mat4.identity();
-        this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(0, -0.1, -0.3)).times(Mat4.scale(0.11, 0.06, 1));
-        this.bird_transform = this.bird_transform.times(Mat4.rotation(Math.PI/2, 0, 0, 1));
-        this.shapes.tri.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("#fcba03")}));
-
-
-        // this.bird_transform = Mat4.identity();
-        // this.bird_transform = Mat4.translation(custom_bird_x, custom_bird_y, custom_bird_z).times(Mat4.translation(1, 1, -1)).times(Mat4.scale(1, 1, 1));
-        // this.shapes.tet.draw(context, program_state, this.bird_transform, this.materials.custom_bird_texture.override({color:hex_color("#fcba03")}));
-        
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            
-
-
         //rotating cannon code:
         let cannon_transform =Mat4.identity();
         let scalebarrel =Mat4.scale(0.5,0.5,2);
@@ -556,7 +487,7 @@ export class Project extends Scene {
             //let birdrotation=rotationYMat.times(rotationXMat);
             let bird_coord = projectile_translations.times(birdrotation.times(birdscale));
             this.shapes.bird.draw(context,program_state,projectile_translations.times(birdrotation.times(birdscale)), this.materials.bird_texture);
-            
+
             //check if collided with box
             //if haven't hit box yet
             if (!this.hit_box){
@@ -579,13 +510,7 @@ export class Project extends Scene {
             if (!this.hit_chocolate){
                 for (let i = 0; i < this.chocolate_list.length; i++){
                     if (this.collision(this.chocolate_list[i],arrayColumn(bird_coord, 3))){
-                        for (let a = 0; a < this.chocolate_keep_list.length; a++){
-                            if (this.chocolate_keep_list[a] == i){
-                                this.chocolate_keep_list.splice(a,1);
-                            }
-                        }
                         this.hit_chocolate = true;
-                        break;
                     }
                 }
             }
@@ -620,14 +545,21 @@ export class Project extends Scene {
         let food = this.add_text.create_text(
             this.add_text.transformation_function([-3.5,-1.3,-5],[0.1,0.1,1]), "Food: "+ this.food_count
         );
-        let game_over = this.add_text.create_text(
-            this.add_text.transformation_function([-3.4,0.4,-5],[0.5,0.5,1]), "Game Over!"
-        );
-        let restart = this.add_text.create_text(
-            this.add_text.transformation_function([-3.4,-0.5,-5],[0.15,0.15,1]), "Press \'m\' to Restart the game!"
+        let game_over_won = this.add_text.create_text(
+            this.add_text.transformation_function([-3.4,0.4,-5],[0.5,0.5,1]), " You Win!"
         );
 
-        if (this.remaining_bird < 0){
+        let game_over_lost = this.add_text.create_text(
+            this.add_text.transformation_function([-3.4,0.5,-5],[0.4,0.4,1]), "  You Lost!"
+        );
+        let game_over_bomb = this.add_text.create_text(
+            this.add_text.transformation_function([-3.4,-0.1,-5],[0.2,0.2,1]), "    You hit the Bomb!"
+        );
+        let restart = this.add_text.create_text(
+            this.add_text.transformation_function([-3.4,-0.5,-5],[0.15,0.15,1]), " Press \'m\' to Restart the game!"
+        );
+
+        if (this.remaining_bird <= 0){
             lives["shape"].set_string("Birds: 0", context.context);
         }
         else{
@@ -644,55 +576,31 @@ export class Project extends Scene {
 
         //display game over
         //if remaining_bird < 0 and food > 1
-        if (this.remaining_bird < 0 && this.food_count > 0){
-            game_over["shape"].set_string(game_over.text, context.context);
-            game_over["shape"].draw(context,program_state,game_over["transform"](program_state.camera_inverse),game_over["material"]);
+        if(this.hit_chocolate){
+            game_over_bomb["shape"].set_string(game_over_bomb.text, context.context);
+            game_over_bomb["shape"].draw(context,program_state,game_over_bomb["transform"](program_state.camera_inverse),game_over_bomb["material"]);
+            game_over_lost["shape"].set_string(game_over_lost.text, context.context);
+            game_over_lost["shape"].draw(context,program_state,game_over_lost["transform"](program_state.camera_inverse),game_over_lost["material"]);
+            restart["shape"].set_string(restart.text, context.context);
+            restart["shape"].draw(context,program_state,restart["transform"](program_state.camera_inverse),restart["material"]);
+            this.game_over = true;
+        }
+
+        else if(this.food_count <= 0){
+            game_over_won["shape"].set_string(game_over_won.text, context.context);
+            game_over_won["shape"].draw(context,program_state,game_over_won["transform"](program_state.camera_inverse),game_over_won["material"]);
+            restart["shape"].set_string(restart.text, context.context);
+            restart["shape"].draw(context,program_state,restart["transform"](program_state.camera_inverse),restart["material"]);
+            this.game_over = true;
+        }
+        else if (this.remaining_bird <= 0 && this.food_count > 0){
+            game_over_lost["shape"].set_string(game_over_lost.text, context.context);
+            game_over_lost["shape"].draw(context,program_state,game_over_lost["transform"](program_state.camera_inverse),game_over_lost["material"]);
             restart["shape"].set_string(restart.text, context.context);
             restart["shape"].draw(context,program_state,restart["transform"](program_state.camera_inverse),restart["material"]);
             this.game_over = true;
         }
         
-    }
-}
-
-
-class Texture_Scroll_X extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
-    fragment_glsl_code() {
-        return this.shared_glsl_code() + `
-            varying vec2 f_tex_coord;
-            uniform sampler2D texture;
-            uniform float animation_time;
-            
-            void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord);
-                if( tex_color.w < .01 ) discard;
-                                                                         // Compute an initial (ambient) color:
-                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                         // Compute the final color with contributions from lights:
-                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-        } `;
-    }
-}
-
-
-class Texture_Rotate extends Textured_Phong {
-    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #7.
-    fragment_glsl_code() {
-        return this.shared_glsl_code() + `
-            varying vec2 f_tex_coord;
-            uniform sampler2D texture;
-            uniform float animation_time;
-            void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord );
-                if( tex_color.w < .01 ) discard;
-                                                                         // Compute an initial (ambient) color:
-                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                         // Compute the final color with contributions from lights:
-                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-        } `;
     }
 }
 
